fe55811d9e38e09dbd0952c39d295354b1a91f52
Dmitry <dmitry@MacBook-Pro.local>
2020-07-03 17:02:13 +0100

    Path change

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import * as pokemonListings from "./modules/pokemonListings";
-import * as pokemonSingle from "./modules/pokemonSingle";
-import * as controls from "./modules/controls";
+import * as pokemonListings from "./pokemonListings";
+import * as pokemonSingle from "./pokemonSingle";
+import * as controls from "./controls";
 
 Vue.use(Vuex);
-
-const debug = process.env.NODE_ENV !== "production";
 
 export default new Vuex.Store({
 	modules: {
 		pokemonListings,
 		pokemonSingle,
 		controls
-	},
-	strict: debug
+	}
 });
